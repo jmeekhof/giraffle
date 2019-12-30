bde57d0ff162fe227f1a9ddf159a9b361670eb10
Josh Meekhof <jmeekhof@twotheleft.com>
2019-12-30 09:28:21 -0500

    Experiment with okhttp

--- src/main/kotlin/com/optum/giraffle/tasks/GsqlTokenTask.kt
+++ src/main/kotlin/com/optum/giraffle/tasks/GsqlTokenTask.kt
@@ -1,13 +1,23 @@
 package com.optum.giraffle.tasks
 
 import khttp.get
+import okhttp3.HttpUrl
+import okhttp3.HttpUrl.Companion.toHttpUrlOrNull
 import org.gradle.api.GradleException
 import org.gradle.api.tasks.TaskAction
+import okhttp3.Request
 
 open class GsqlTokenTask() : GsqlTokenAbstract() {
 
     @TaskAction
     fun initToken() {
+
+        val urlBuilder = url().toHttpUrlOrNull().newBuilder()
+
+        val httpUrl: HttpUrl.Builder = HttpUrl.parse(url()).newBuilder()
+            .query("secret=${gsqlPluginExtension.authSecret.get()}")
+        val r = Request.Builder()
+            .url(url())
 
         val r = get(url = url(), params = mapOf(
             "secret" to gsqlPluginExtension.authSecret.get()
@@ -15,7 +25,7 @@ open class GsqlTokenTask() : GsqlTokenAbstract() {
         with(logger) {
             // info("generated url: {}", r.url)
             info("status code: {}", r.statusCode)
-            info("response: {}", r.jsonObject)
+            info("response: {}", r.jsonObject
         }
 
         when (r.jsonObject["error"]) {
